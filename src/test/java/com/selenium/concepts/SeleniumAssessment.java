019b5cdfbcee69184d5a2e309b5d4bdc03a9e2a0
BalajiSaravanan210697 <balajisaravanan210697@gmail.com>
2023-09-09 16:37:06 +0530

    Selenium Assessment

--- src/test/java/com/selenium/concepts/SeleniumAssessment.java
+++ src/test/java/com/selenium/concepts/SeleniumAssessment.java
@@ -1,20 +1,24 @@
 package com.selenium.concepts;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class SeleniumAssessment {
 
 	
 
-	@SuppressWarnings("deprecation")
+	
 	public static void main(String[] args) throws InterruptedException {
 
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
 		/* Step1: Load application url https://api-training.atlassian.net/ */
 		driver.get("https://api-training.atlassian.net/");
 		// Step2: Enter your email hari.radhakrishnan@testleaf.com and click on Continue
@@ -25,6 +29,7 @@ public class SeleniumAssessment {
 		driver.findElement(By.xpath("//span[text()='Log in']")).click();
 		// Step4: Select SDET-5 project
         driver.findElement(By.xpath("//p[text()='SDET-5']")).click();
+        Thread.sleep(1000);
         //Step6: Click on Create button
         driver.findElement(By.xpath("//span[text()='Create']")).click();
         //Step7: Enter Summary which is mandatory
@@ -35,8 +40,13 @@ public class SeleniumAssessment {
         driver.findElement(By.xpath("//span[text()='Backlog']")).click();
         //Step10: Search for the story we created in the search field and verify the newly created story
         driver.findElement(By.xpath("//input[@name='search']")).sendKeys("Story Created By Balaji S");
-		Thread.sleep(1000);
+        Thread.sleep(1000);
+        String text= driver.findElement(By.xpath("//mark")).getText();
+        System.out.println(text);
+        Assert.assertTrue(text.contains("Balaji"));
+	
 
 	}
+
 
 }
